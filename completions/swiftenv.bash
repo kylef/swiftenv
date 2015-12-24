c507d071502dc1a5574c30967f6d286134223d36
Kyle Fuller <kyle@fuller.li>
2015-12-24 00:24:20 +0000

    Add support for completions on bash

---
+++ completions/swiftenv.bash
@@ -0,0 +1,16 @@
+_swiftenv() {
+  COMPREPLY=()
+  local word="${COMP_WORDS[COMP_CWORD]}"
+
+  if [ "$COMP_CWORD" -eq 1 ]; then
+    COMPREPLY=( $(compgen -W "$(swiftenv commands)" -- "$word") )
+  else
+    local words=("${COMP_WORDS[@]}")
+    unset words[0]
+    unset words[$COMP_CWORD]
+    local completions=$(swiftenv completions "${words[@]}")
+    COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+  fi
+}
+
+complete -F _swiftenv swiftenv
